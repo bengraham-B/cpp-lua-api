387bf4bb53471cfd877c06b5b28210a3afd81c30
bengraham-B <grahamben7@gmail.com>
2024-04-24 15:34:23 +0200

    Need to findout what is happening with the JSON module

---
+++ post.lua
@@ -0,0 +1,39 @@
+local http = require("socket.http")
+local json = require("json")
+print("starting POST")
+
+local data = {
+    name = "Goose McQuack",
+    age = 21,
+    language = 'JS'
+}
+
+local request_body = json.encode(data)
+
+local options = {
+    url = "http://localhost:8001/data",
+    method = "POST",
+    headers = {
+        ["Content-Type"] = "application/json",
+        ["Content-Length"] = tostring(#request_body)
+    },
+
+    source = ltn12.source.string(request_body)
+}
+
+local response_body = {}
+local _, status_code, _ = http.request{
+    url = options.url,
+    method = options.method,
+    headers = options.headers,
+    source = options.source,sink = ltn12.sink.table(response_body)
+}
+
+if status_code == 200 then 
+    print("Request Successful")
+print(table.concat(response_body))
+else 
+ print("Failed:", status_code)
+end
+
+print("Ending POST")
--- server.ts
+++ server.ts
@@ -13,6 +13,14 @@ app.get("/test", (req:Request, res:Response) => {
     res.status(200).send("Express + Typescript Server!")
 })
 
+app.post("/data", (req:Request, res: Response) => {
+    console.log("POST")
+    const {data} = req.body 
+    console.log(data)
+
+    res.status(200).json({msg: "POST Route"})
+})
+
 app.listen(port, () => {
     console.log("[Server] running PORT:8001")
 })
